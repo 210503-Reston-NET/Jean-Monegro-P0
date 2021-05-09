ce32b4f41b24165baadc6ee72aa95ae1b4c44968
Jean Monegro <jeanmonegro1@gmail.com>
2021-05-08 21:40:58 -0700

    5/8

---
+++ ProjectP0/Store.cs
@@ -0,0 +1,14 @@
+namespace ProjectP0
+{
+    public class newclass
+    {
+        public string Name{get;set;}
+
+        public string City{get;set;}
+
+        public string State{get;set;}
+
+        public string Review{get;set;}
+
+    }
+}
