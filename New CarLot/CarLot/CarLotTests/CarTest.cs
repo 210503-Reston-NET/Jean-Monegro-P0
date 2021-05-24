bd6dedeb254849e4e5d3df9b44c8e278c3c3743f
Jean Monegro <jeanmonegro1@gmail.com>
2021-05-24 10:53:33 -0700

    main

--- New CarLot/CarLot/CarLotTests/CarTest.cs
+++ New CarLot/CarLot/CarLotTests/CarTest.cs
@@ -1,3 +1,9 @@
+using Entity = CarLotDL.Entities;
+using Model = CarLotModels;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using CarLotDL;
+using System.Linq;
 using System;
 using Xunit;
 using CarLotModels;
@@ -5,6 +11,31 @@ namespace CarLotTests
 {
     public class CarTest
     {
+
+        private readonly DbContextOptions<Entity.PracticeContext> options;
+
+        [Fact]
+        public void GetCarShouldReturnCar()
+        {
+            using (var context = new Entity.PracticeContext(options))
+            {
+                IRepository _repo = new RepoDB(context);
+                var cars = _repo.GetAllCars();
+            }
+        }
+        // [Fact]
+        // public void AddLocationShouldAddLocation()
+        // {
+        //     using(var context = new Entity.PracticeContext(options))
+        //     {
+        //         Location _repo = new RepoDB(context);
+        //         _repo.AddLocation
+        //         (
+        //             new Model.Location("Ciy", "State", "USA")
+        //         );
+        //     }
+        //     using(var assertContext = new Entity.PracticeContext(options))
+        // }
         [Fact]
         public void ModelShouldSetValidData()
         {
@@ -23,6 +54,19 @@ namespace CarLotTests
             Car test = new Car();
 
             Assert.Throws<Exception>(() => test.Model = input);
+        }
+        [Fact]
+        public void AddCarShouldAddCar()
+        {
+            using (var context = new Entity.PracticeContext(options))
+            {
+                IRepository _repo = new RepoDB(context);
+                //Act with a test context
+                _repo.AddCar
+                (
+                    new Model.Car("Scion", "tc", 2008)
+                );
+            }
         }
     }
 }
